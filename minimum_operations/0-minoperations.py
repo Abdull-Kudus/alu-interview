588c3732b3b5a91a2ca367ef6a38aa8c3fa455bc
Abdull-Kudus <a.zakariam@alustudent.com>
2026-01-14 06:21:09 +0200

    fix: pep8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -9,12 +9,16 @@ def minOperations(n):
         Calculates the fewest number of operation needed
         to result exactly n H characters in the file
     """
-
-    if not n or n <= 1:
+    if n <= 1:
         return 0
+
     operations = 0
-    for time in range(2, n+1):
-        while(n % time == 0):
-            operations += time
-            n = n / time
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor  # Use integer division
+        divisor += 1
+
     return operations
